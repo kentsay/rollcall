a1b9f4419ecee8e178e61d2ac64a6a7c8a6ad120
Ken Tsay <naturen@gmail.com>
2015-03-27 01:14:32 +0100

    update css

--- app/views/rollcall_query.php
+++ app/views/rollcall_query.php
@@ -11,6 +11,7 @@
   <!-- CSS -->
   <link href="https://knt.wufoo.com/stylesheets/public/forms/css/index.0101.css" rel="stylesheet">
   <link href="http://128.199.158.31/css/menu.css" rel="stylesheet">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
   <!-- js -->
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
