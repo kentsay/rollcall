b64d962e9b62850c735d73e3adaf029bb0ffc2c3
Ken Tsay <naturen@gmail.com>
2016-04-08 08:23:54 -0400

    init push for http library

---
+++ app/library/HttpUtil.php
@@ -0,0 +1,19 @@
+<?php
+
+class HttpUtil {
+
+  /*  Set Request param  
+   */
+  public static function setRequestData() {
+  
+  }
+
+  public function checkRequestData($requestData) {
+    $paramArr = Request::all();
+    foreach($requestData as $request) {
+
+    }
+  }
+}
+
+?>
