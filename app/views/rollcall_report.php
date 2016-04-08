257588a3476c3727df13be07d7b96eb66b4d3606
kentsay <naturen@gmail.com>
2016-04-08 15:18:35 +0200

    revert

--- app/views/rollcall_report.php
+++ app/views/rollcall_report.php
@@ -1,7 +1,3 @@
-<?php
-require_once '../library/HttpUtil.php';
-?>
-
 <html>
 <head>
   <title>訓練報表</title>
@@ -33,9 +29,36 @@ if (!empty($param['hall']))
 if (!empty($param['bgroup']))
   $url .= "&bgroup=".$param['bgroup'];
 
-$response = HttpUtil.get_web_page($url);
+$response = get_web_page($url);
 $resArr = array();
 $resArr = json_decode($response);
+
+function get_web_page($url) {
+  $options = array (CURLOPT_RETURNTRANSFER => true, // return web page
+    CURLOPT_HEADER => false, // don't return headers
+    CURLOPT_FOLLOWLOCATION => true, // follow redirects
+    CURLOPT_ENCODING => "", // handle compressed
+    CURLOPT_USERAGENT => "test", // who am i
+    CURLOPT_AUTOREFERER => true, // set referer on redirect
+    CURLOPT_CONNECTTIMEOUT => 120, // timeout on connect
+    CURLOPT_TIMEOUT => 120, // timeout on response
+    CURLOPT_MAXREDIRS => 10 ); // stop after 10 redirects
+
+  $ch = curl_init ( $url );
+  curl_setopt_array ( $ch, $options );
+  $content = curl_exec ( $ch );
+  $err = curl_errno ( $ch );
+  $errmsg = curl_error ( $ch );
+  $header = curl_getinfo ( $ch );
+  $httpCode = curl_getinfo ( $ch, CURLINFO_HTTP_CODE );
+
+  curl_close ( $ch );
+
+  $header ['errno'] = $err;
+  $header ['errmsg'] = $errmsg;
+  $header ['content'] = $content;
+  return $header ['content'];
+}
 
 function convertStatus($record,&$ontime,&$late,&$absent,&$leave) {
   switch($record) {
